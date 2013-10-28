b6c62a0c11206e5a90d9891f14c5a21e2629e8e3
LauHinlam <chiliplau@gmail.com>
2013-10-28 12:15:47 +0800

    version 0

---
+++ README.md
@@ -0,0 +1,6 @@
+These two small tools may useful when reading source code.
+Both these two python script acquire a keyword when it start, then search all files in current directory and all of its subdirectory.
+
+Search_Content.py can search all files content. If there is a piece of string match keyword, filename will be written into "result.txt".
+
+Search_Title.py search title. Just as Search_Content.py, if match keyword, filename will be written into "result.txt".
---
+++ Search_Title.py
@@ -0,0 +1,46 @@
+#coding:utf-8
+
+import os
+from time import clock
+
+def All_File_List(path):
+	filelist = []
+	direlist = []
+	All_File = os.listdir(path)
+	for items in All_File:
+		if os.path.isfile(path + '/' + items):
+			filelist.append(path + '/' + items)
+		else:
+			direlist.append(path + '/' + items)
+	return filelist, direlist
+
+def Search(f, keyword, path):
+	filelist, direlist = All_File_List(path)
+	for files in filelist:
+		if files.find(keyword) > -1:
+			f.writelines(files + '\n')
+	for direc in direlist:
+		if direc.find(keyword) > -1:
+			f.writelines(direc + '\n')
+		Search(f, keyword, direc)
+
+def main():
+	keyword = raw_input('Please input keyword:')
+	start = clock()
+	if not keyword:
+		print 'Keyword cannot be empty!'
+		return
+	f = open('result.txt', 'w')
+	f.writelines('Keyword:' + keyword + '\n\n')
+	status = Search(f, keyword, '.')
+	if False == status:
+		print 'Sorry, no result found.'
+	else:
+		print 'Done.'
+	end = clock()
+	print 'Cost time: %f seconds.' %(end - start)
+	f.writelines('\n')
+	f.close()
+
+if __name__ == '__main__':
+	main()
